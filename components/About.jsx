b288b66001a40615aee770818c117e98d8a231ac
ajeetfresco <ajeet@frescowebservices.com>
2024-04-24 17:01:13 +0530

    add shadows

--- components/About.jsx
+++ components/About.jsx
@@ -13,16 +13,16 @@ const About = () => {
           </p>
           <h2 className='py-4 dark:text-gray-300'>Who I Am</h2>
           <p className='py-2 pb-4 text-gray-600 dark:text-gray-400'>
-            I'm your front-end powerhouse, specializing in crafting mobile-friendly UIs that seamlessly integrate with backend systems. With a passion for learning and a knack for picking up new tech, I'm always ready to tackle whatever comes my way.
+            I{`'`}m your front-end powerhouse, specializing in crafting mobile-friendly UIs that seamlessly integrate with backend systems. With a passion for learning and a knack for picking up new tech, I{`'`}m always ready to tackle whatever comes my way.
           </p>
           <p className='py-2 pb-4 text-gray-600 dark:text-gray-400'>
-            From HTML to CSS to JavaScript, with React as my go-to tool, I've got the skills to bring your vision to life. And if there's a new tech stack on the horizon, I'll dive right in and make it my own.
+            From HTML to CSS to JavaScript, with React as my go-to tool, I{`'`}ve got the skills to bring your vision to life. And if there{`'`}s a new tech stack on the horizon, I{`'`}ll dive right in and make it my own.
             </p>
           <p className='py-2 pb-4 text-gray-600 dark:text-gray-400'>
-          In my world, there's no one-size-fits-all solution. I believe in choosing the right tool for the job, whether it's mastering APIs or delving into backend wizardry.
+          In my world, there{`'`}s no one-size-fits-all solution. I believe in choosing the right tool for the job, whether it{`'`}s mastering APIs or delving into backend wizardry.
           </p>
           <p className='py-2 pb-4 text-gray-600 dark:text-gray-400'>
-          Why choose me? Because I'm not just a developer—I'm a problem-solver, a creative thinker, and a team player ready to take on any challenge. Let's join forces and create something extraordinary.
+          Why choose me? Because I{`'`}m not just a developer—I{`'`}m a problem-solver, a creative thinker, and a team player ready to take on any challenge. Let{`'`}s join forces and create something extraordinary.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer dark:text-gray-400'>
